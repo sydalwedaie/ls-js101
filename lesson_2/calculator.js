4233dc11b2ff1651adab39672e8d635c574f9f65
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-22 06:19:32 +0300

    Complete lesson 2 calculator project

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,25 +1,36 @@
-let rlSync = require('readline-sync');
-let num1 = rlSync.question("Enter first number");
-let num2 = rlSync.question("Enter second number");
-let op = rlSync.question("Enter type of operation");
+const rlSync = require('readline-sync');
+
+console.log('\nWelcome to Calculator!')
+console.log('----------------------------')
+
+let num1 = Number(rlSync.question("Enter first number:\n"));
+let num2 = Number(rlSync.question("Enter second number:\n"));
+let op = rlSync.question("Enter type of operation:\n1) Add 2) Subtract 3) Multiply 4) Divide\n");
 
 let result;
+let opSign;
 
 switch(op) {
-  case '+':
+  case '1':
     result = num1 + num2;
+    opSign = '+'
     break;
-  case '-':
+  case '2':
     result = num1 - num2;
+    opSign = '-'
     break;
-  case '*':
+  case '3':
     result = num1 * num2;
+    opSign = '*'
     break;
-  case '/':
+  case '4':
     result = num1 / num2;
+    opSign = '/'
     break;
   default:
     console.log("Operation not valid");
 }
 
-console.log(`${num1} ${op} ${num2} = ${result}`);
+console.log('----------------------------')
+if (opSign) console.log(`${num1} ${opSign} ${num2} = ${result}`);
+
