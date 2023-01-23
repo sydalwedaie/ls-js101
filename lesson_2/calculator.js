160ee456905394d294a28634110b233668f59a7f
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-23 04:30:39 +0300

    Rectify lesson 2 calculator ESlint errors

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,7 +1,7 @@
 const rlSync = require('readline-sync');
 
-console.log('\nWelcome to Calculator!')
-console.log('----------------------------')
+console.log('\nWelcome to Calculator!');
+console.log('----------------------------');
 
 let num1 = Number(rlSync.question("Enter first number:\n"));
 let num2 = Number(rlSync.question("Enter second number:\n"));
@@ -10,27 +10,27 @@ let op = rlSync.question("Enter type of operation:\n1) Add 2) Subtract 3) Multip
 let result;
 let opSign;
 
-switch(op) {
+switch (op) {
   case '1':
     result = num1 + num2;
-    opSign = '+'
+    opSign = '+';
     break;
   case '2':
     result = num1 - num2;
-    opSign = '-'
+    opSign = '-';
     break;
   case '3':
     result = num1 * num2;
-    opSign = '*'
+    opSign = '*';
     break;
   case '4':
     result = num1 / num2;
-    opSign = '/'
+    opSign = '/';
     break;
   default:
     console.log("Operation not valid");
 }
 
-console.log('----------------------------')
+console.log('----------------------------');
 if (opSign) console.log(`${num1} ${opSign} ${num2} = ${result}`);
 
