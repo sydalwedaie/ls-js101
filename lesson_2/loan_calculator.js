efaffe5e5160c875005f9c1df9cd84edb53dd3f2
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-31 07:43:18 +0300

    Convert all inputs to make use of getInput function.

--- lesson_2/loan_calculator.js
+++ lesson_2/loan_calculator.js
@@ -42,38 +42,18 @@ prompt("Welcome to Loan Calculator!\n   ---------------------------");
 
 while (true) {
   principal = getInput("Please enter the loan amount greater than 0 in USD:");
-
-  prompt("Please enter the APR in percentage points:");
-  while (true) {
-    monthlyRate = parseFloat(rlSync.prompt()) / 1200;
-    if (monthlyRate || monthlyRate === 0) break;
-    else prompt("Invalid entry.");
-  }
+  monthlyRate = getInput("Please enter the APR in percentage points:") / 1200;
 
   prompt("Please enter the loan duration");
-  prompt("Enter the number of years (whole numbers):");
-  while (true) {
-    durationMonths = parseFloat(rlSync.prompt()) * 12;
-    if (durationMonths || durationMonths === 0) break;
-    else prompt("Invalid entry.");
-  }
-
-  prompt("Enter the number of months (whole numbers):");
-  while (true) {
-    let input = parseFloat(rlSync.prompt());
-    if (durationMonths === 0 && input === 0) {
-      prompt("The duration must be at least 1 month.");
-    } else if (input === 0 || input) {
-      durationMonths += input;
-      break;
-    } else prompt("Invalid entry.");
-  }
+  durationMonths = getInput("Enter the number of years:") * 12;
+  durationMonths += getInput("Enter the number of months:");
 
   monthlyPayment = computeMonthlyPayment(
     principal,
     monthlyRate,
     durationMonths
   );
+
   totalPayment = durationMonths * monthlyPayment;
   totalInterest = totalPayment - principal;
 
