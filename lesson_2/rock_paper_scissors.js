11549d854a9572c02cde5e45621bf42f3a39c61e
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-10 13:28:29 +0300

    RPS: Complete walkthrough

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -5,39 +5,69 @@ If player a chooses paper and player b chooses rock, player a wins.
 If player a chooses scissors and player b chooses paper, player a wins.
 If both players choose the same item, neither player wins. It's a tie.
 
-rock > scissors
-paper > rock
-scissors > paper
+rock (0) > scissors (2)
+paper (1) > rock (0)
+scissors (2) > paper (1)
 */
 
 const readline = require("readline-sync");
+const VALID_CHOICES = ["rock", "paper", "scissors"];
 
-let userChoice = Number(
-  readline.question("Choose: 1) Rock 2) Paper 3) Scissors -> ")
-);
-let computerChoice = Math.floor(Math.random() * 3) + 1;
-
-function determineWinner(playerA, playerB) {
-  if (playerA === playerB) return "tie";
-  else if (
-    (playerA === 1 || playerB === 1) &&
-    (playerA === 3 || playerB === 3)
-  ) {
-    return playerA < playerB ? "playerA" : "playerB";
-  } else if (
-    (playerA === 2 || playerB === 2) &&
-    (playerA === 1 || playerB === 1)
-  ) {
-    return playerA > playerB ? "playerA" : "playerB";
-  } else if (
-    (playerA === 3 || playerB === 3) &&
-    (playerA === 2 || playerB === 2)
-  ) {
-    return playerA > playerB ? "playerA" : "playerB";
-  }
-  return null;
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(`You chose: ${userChoice}`);
-console.log(`Computer chose: ${computerChoice}`);
-console.log(determineWinner(userChoice, computerChoice));
+function getUserChoice() {
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let userChoice = readline.question();
+
+  while (!VALID_CHOICES.includes(userChoice)) {
+    prompt("That's not a valid choice");
+    userChoice = readline.question();
+  }
+  return userChoice;
+}
+
+function displayWinner(playerA, playerB) {
+  if (
+    (playerA === VALID_CHOICES[0] && playerB === VALID_CHOICES[2]) ||
+    (playerA === VALID_CHOICES[1] && playerB === VALID_CHOICES[0]) ||
+    (playerA === VALID_CHOICES[2] && playerB === VALID_CHOICES[1])
+  ) {
+    prompt("You win!");
+  } else if (
+    (playerA === VALID_CHOICES[2] && playerB === VALID_CHOICES[0]) ||
+    (playerA === VALID_CHOICES[0] && playerB === VALID_CHOICES[1]) ||
+    (playerA === VALID_CHOICES[1] && playerB === VALID_CHOICES[2])
+  ) {
+    prompt("Computer wins!");
+  } else {
+    prompt("It's a tie!");
+  }
+}
+
+function newGame() {
+  prompt("Another Game? y/n");
+  let anotherGame = readline.question().trim()[0].toLowerCase();
+  while (anotherGame !== "y" && anotherGame !== "n") {
+    prompt("Please enter y or n");
+    anotherGame = readline.question().trim()[0].toLowerCase();
+  }
+  if (anotherGame === "n") {
+    prompt("Thank you for playing.");
+    return false;
+  }
+  return true;
+}
+
+do {
+  console.clear();
+  let userChoice = getUserChoice();
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose: ${userChoice}, computer chose: ${computerChoice}`);
+
+  displayWinner(userChoice, computerChoice);
+} while (newGame());
