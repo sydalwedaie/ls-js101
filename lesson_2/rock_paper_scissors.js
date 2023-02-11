b5ea4870e5f22f7d8df9307787c3c5d8f564534f
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-11 14:52:31 +0300

    RPS: Minor tidy up. Add messages object. Add welcom sequence. Add colorfull prompts.

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,5 +1,6 @@
 const readline = require("readline-sync");
 const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
+const FINISHING_SCORE = 3;
 
 // keys are set to an array of choices they can beat
 const RULES = {
@@ -10,6 +11,23 @@ const RULES = {
   spock: ["rock", "scissors"],
 };
 
+let messages = {
+  welcom: "Welcome to the RPS game, enhanced edition!",
+  instructions:
+    "This is similar to the classic RPS game with two extra objects: lizard and spock. Each object can beat two other objects. The rules are listed in the table below:",
+  startPrompt: "Enter any key to start",
+  enterChoice: `Choose one: ${VALID_CHOICES.join(", ")}`,
+  choiceInstruction:
+    "You can either enter the full name or the first two letters",
+  invalidChoice: "That's not a valid choice",
+  userWins: "Wahooo! You win!",
+  computerWins: "Gosh! Computer wins!",
+  tie: "It's a tie!",
+  playAgain: "Play again? y/n",
+  playAgainError: "Please enter y or n",
+  farewell: "Thank you for playing!",
+};
+
 let roundNumber = 1;
 let userScore = 0;
 let computerScore = 0;
@@ -18,18 +36,36 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function promptError(message) {
+  console.log("\x1b[31m%s\x1b[0m", `=> ${message}`);
+}
+
+function promptInfo(message) {
+  console.log("\x1b[36m%s\x1b[0m", `=> ${message}`);
+}
+
+function promptResult(message) {
+  console.log("\x1b[33m%s\x1b[0m", `=> ${message}`);
+}
+
+function displayWelcom() {
+  console.clear();
+  prompt(messages.welcom);
+  prompt(messages.instructions);
+  console.table(RULES);
+  prompt(messages.startPrompt);
+}
+
 function getUserChoice() {
-  prompt(
-    `Choose one: ${VALID_CHOICES.join(
-      ", "
-    )}\n   (You can also enter the first two letters)`
-  );
-  let input = readline.question();
+  prompt(messages.enterChoice);
+  prompt(messages.choiceInstruction);
+
+  let input = readline.question().toLowerCase();
   let userChoice = getFullName(input) || input;
 
   while (!VALID_CHOICES.includes(userChoice)) {
-    prompt("That's not a valid choice");
-    input = readline.question();
+    promptError(messages.invalidChoice);
+    input = readline.question().toLowerCase();
     userChoice = getFullName(input) || input;
   }
   return userChoice;
@@ -61,56 +97,71 @@ function play(playerA, playerB) {
 }
 
 function displayResults(userScore, computerScore) {
-  if (userScore > computerScore) prompt("You win!");
-  else if (userScore < computerScore) prompt("Computer wins!");
-  else prompt("It's a tie!");
-
-  prompt(`Your score: ${userScore}`);
-  prompt(`Computer score: ${computerScore}`);
+  if (userScore > computerScore) promptResult(messages.userWins);
+  else if (userScore < computerScore) promptResult(messages.computerWins);
+  else promptResult(messages.tie);
 }
 
 function displayEndGame(roundNumber, userScore, computerScore) {
-  if (userScore === 3) {
-    prompt(`You beat the computer in ${roundNumber} rounds!`);
-  } else if (computerScore === 3) {
-    prompt(`The computer beat you in ${roundNumber} rounds! Game Over!`);
+  console.clear();
+  displayScoreboard(roundNumber, userScore, computerScore);
+  if (userScore === FINISHING_SCORE) {
+    promptResult(`You beat the computer in ${roundNumber} rounds!`);
+  } else if (computerScore === FINISHING_SCORE) {
+    promptResult(`The computer beat you in ${roundNumber} rounds! Game Over!`);
   }
+  prompt(messages.farewell);
+}
+
+function displayScoreboard(roundNumber, userScore, computerScore) {
+  promptInfo(`Round Number: ${roundNumber}`);
+  promptInfo(`Your score: ${userScore}`);
+  promptInfo(`Computer score: ${computerScore}`);
+  prompt("------------------");
 }
 
 function newGame() {
   roundNumber += 1;
 
-  prompt("Another Game? y/n");
-  let anotherGame = readline.question().trim().toLowerCase()[0];
+  prompt(messages.playAgain);
+  let playAgain = readline.question().trim().toLowerCase();
 
-  while (anotherGame !== "y" && anotherGame !== "n") {
-    prompt("Please enter y or n");
-    anotherGame = readline.question().trim().toLowerCase()[0];
+  while (playAgain !== "y" && playAgain !== "n") {
+    promptError(messages.playAgainError);
+    playAgain = readline.question().trim().toLowerCase();
   }
 
-  if (anotherGame === "n") {
-    prompt("Thank you for playing.");
+  if (playAgain === "n") {
+    prompt(messages.farewell);
     return false;
   }
 
   return true;
 }
 
-do {
-  console.clear();
-  prompt(`Round Number: ${roundNumber}`);
-  let userChoice = getUserChoice();
+function startGame() {
+  do {
+    console.clear();
+    displayScoreboard(roundNumber, userScore, computerScore);
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+    let userChoice = getUserChoice();
 
-  prompt(`You chose: ${userChoice}, computer chose: ${computerChoice}`);
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
 
-  play(userChoice, computerChoice);
-  displayResults(userScore, computerScore);
+    prompt(`You chose: ${userChoice}, computer chose: ${computerChoice}`);
 
-  if (userScore === 3 || computerScore === 3) {
-    displayEndGame(roundNumber, userScore, computerScore);
-    break;
-  }
-} while (newGame());
+    play(userChoice, computerChoice);
+    displayResults(userScore, computerScore);
+
+    if (userScore === FINISHING_SCORE || computerScore === FINISHING_SCORE) {
+      displayEndGame(roundNumber, userScore, computerScore);
+      break;
+    }
+  } while (newGame());
+}
+
+displayWelcom();
+
+let start = readline.question();
+if (start || start.trim() === "") startGame();
