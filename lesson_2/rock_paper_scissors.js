415fb1677c51e1169c4f41d6cd60db988e2ac79d
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-11 15:11:03 +0300

    RPS: Add clear last lines logic. Reshuffle code structure.

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIABLES
 const readline = require("readline-sync");
 const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
 const FINISHING_SCORE = 3;
@@ -14,7 +15,7 @@ const RULES = {
 let messages = {
   welcom: "Welcome to the RPS game, enhanced edition!",
   instructions:
-    "This is similar to the classic RPS game with two extra objects: lizard and spock. Each object can beat two other objects. The rules are listed in the table below:",
+    "This is similar to the classic RPS game with two extra objects: lizard and spock. Each object can beat two other objects. The rules are listed in the table below. The first palyer to win 3 times wins the game.",
   startPrompt: "Enter any key to start",
   enterChoice: `Choose one: ${VALID_CHOICES.join(", ")}`,
   choiceInstruction:
@@ -24,7 +25,7 @@ let messages = {
   computerWins: "Gosh! Computer wins!",
   tie: "It's a tie!",
   playAgain: "Play again? y/n",
-  playAgainError: "Please enter y or n",
+  playAgainError: "Invalid input. Please enter y or n",
   farewell: "Thank you for playing!",
 };
 
@@ -32,6 +33,7 @@ let roundNumber = 1;
 let userScore = 0;
 let computerScore = 0;
 
+// HELPER FUNCTIONS
 function prompt(message) {
   console.log(`=> ${message}`);
 }
@@ -48,6 +50,12 @@ function promptResult(message) {
   console.log("\x1b[33m%s\x1b[0m", `=> ${message}`);
 }
 
+function clearLastLines(count) {
+  process.stdout.moveCursor(0, -count);
+  process.stdout.clearScreenDown();
+}
+
+// DISPLAY LOGIC
 function displayWelcom() {
   console.clear();
   prompt(messages.welcom);
@@ -56,6 +64,31 @@ function displayWelcom() {
   prompt(messages.startPrompt);
 }
 
+function displayResults(userScore, computerScore) {
+  if (userScore > computerScore) promptResult(messages.userWins);
+  else if (userScore < computerScore) promptResult(messages.computerWins);
+  else promptResult(messages.tie);
+}
+
+function displayEndGame(roundNumber, userScore, computerScore) {
+  console.clear();
+  displayScoreboard(roundNumber, userScore, computerScore);
+  if (userScore === FINISHING_SCORE) {
+    promptResult(`You beat the computer in ${roundNumber} rounds!`);
+  } else if (computerScore === FINISHING_SCORE) {
+    promptResult(`The computer beat you in ${roundNumber} rounds! Game Over!`);
+  }
+  prompt(messages.farewell);
+}
+
+function displayScoreboard(roundNumber, userScore, computerScore) {
+  promptInfo(`Round Number: ${roundNumber}`);
+  promptInfo(`Your score: ${userScore}`);
+  promptInfo(`Computer score: ${computerScore}`);
+  prompt("------------------");
+}
+
+// GAME LOGIC
 function getUserChoice() {
   prompt(messages.enterChoice);
   prompt(messages.choiceInstruction);
@@ -64,6 +97,7 @@ function getUserChoice() {
   let userChoice = getFullName(input) || input;
 
   while (!VALID_CHOICES.includes(userChoice)) {
+    clearLastLines(2);
     promptError(messages.invalidChoice);
     input = readline.question().toLowerCase();
     userChoice = getFullName(input) || input;
@@ -96,30 +130,6 @@ function play(playerA, playerB) {
   }
 }
 
-function displayResults(userScore, computerScore) {
-  if (userScore > computerScore) promptResult(messages.userWins);
-  else if (userScore < computerScore) promptResult(messages.computerWins);
-  else promptResult(messages.tie);
-}
-
-function displayEndGame(roundNumber, userScore, computerScore) {
-  console.clear();
-  displayScoreboard(roundNumber, userScore, computerScore);
-  if (userScore === FINISHING_SCORE) {
-    promptResult(`You beat the computer in ${roundNumber} rounds!`);
-  } else if (computerScore === FINISHING_SCORE) {
-    promptResult(`The computer beat you in ${roundNumber} rounds! Game Over!`);
-  }
-  prompt(messages.farewell);
-}
-
-function displayScoreboard(roundNumber, userScore, computerScore) {
-  promptInfo(`Round Number: ${roundNumber}`);
-  promptInfo(`Your score: ${userScore}`);
-  promptInfo(`Computer score: ${computerScore}`);
-  prompt("------------------");
-}
-
 function newGame() {
   roundNumber += 1;
 
@@ -127,6 +137,7 @@ function newGame() {
   let playAgain = readline.question().trim().toLowerCase();
 
   while (playAgain !== "y" && playAgain !== "n") {
+    clearLastLines(2);
     promptError(messages.playAgainError);
     playAgain = readline.question().trim().toLowerCase();
   }
@@ -161,6 +172,7 @@ function startGame() {
   } while (newGame());
 }
 
+// RUN GAME
 displayWelcom();
 
 let start = readline.question();
