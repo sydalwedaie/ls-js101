9eb5a3b5ba474a9ba0aa93bbec4c4f5d6275ad76
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-13 22:45:48 +0300

    Complete Lesson 3 Easy 1 Q04

---
+++ lesson_3/easy_1/q04.js
@@ -0,0 +1,12 @@
+/* Using the following string, create a new string that contains
+all lowercase letters except for the first character, which should
+be capitalized. (See the example output.)
+ */
+
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+console.log(
+  munstersDescription[0].toUpperCase() +
+    munstersDescription.slice(1).toLocaleLowerCase()
+);
