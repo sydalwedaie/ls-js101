537dc4a571a4ec3dab0605fe7584c10b75218bea
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-15 18:36:55 +0300

    Complete Lesson 3 Easy 3 Q05

---
+++ lesson_3/easy_3/q05.js
@@ -0,0 +1,21 @@
+/*
+The following function unnecessarily uses two return statements to return
+boolean values. Can you rewrite this function so it only has one return
+statement and does not explicitly use either true or false?
+
+function isColorValid(color) {
+  if (color === "blue" || color === "green") {
+    return true;
+  } else {
+    return false;
+  }
+}
+*/
+
+function isColorValid(color) {
+  return color === "blue" || color === "green";
+}
+
+console.log(isColorValid("blue")); // true
+console.log(isColorValid("green")); // true
+console.log(isColorValid("red")); // false
