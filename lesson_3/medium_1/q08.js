a7f01c7424b3638dfebe09509a029808dd671184
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-16 01:07:05 +0300

    Complete Lesson 3 Medium 1 Q08

---
+++ lesson_3/medium_1/q08.js
@@ -0,0 +1,21 @@
+// Is the family data changed by running the program?
+
+let munsters = {
+  Herman: { age: 32, gender: "male" },
+  Lily: { age: 30, gender: "female" },
+  Grandpa: { age: 402, gender: "male" },
+  Eddie: { age: 10, gender: "male" },
+  Marilyn: { age: 23, gender: "female" },
+};
+
+function messWithDemographics(demoObject) {
+  Object.values(demoObject).forEach(familyMember => {
+    familyMember["age"] += 42;
+    familyMember["gender"] = "other";
+  });
+}
+
+messWithDemographics(munsters);
+
+// Yes. The object is passed by reference and the function performs a mutating
+// operation on it.
