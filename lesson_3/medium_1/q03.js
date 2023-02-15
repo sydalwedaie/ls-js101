808bceaade1d18d13fd8a1e00605bdbb99ccfa46
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-15 20:12:31 +0300

    Complete Lesson 3 Medium 1 Q03

---
+++ lesson_3/medium_1/q03.js
@@ -0,0 +1,30 @@
+/*
+Fix the edge case for zero and negative numbers
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  do {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  } while (divisor !== 0);
+  return factors;
+}
+*/
+
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+console.log(factors(-4));
+console.log(factors(0));
+console.log(factors(15));
